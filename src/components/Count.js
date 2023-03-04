ceecf63c21a0e8292476f7dfb6994c8027916aea
Vladislow <t1nky132@mail.ru>
2023-03-04 15:02:43 +0500

    counter value

---
+++ src/components/Count.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './count.css';
+
+function Count(){
+    return(
+        <section class='count__section'>
+            <h1 class='count__value'>0</h1>
+        </section>
+    );
+}
+
+export default Count;
